d55c25c14df13a7ed823e4018b75081e5aa47c4b
walkerpig <walkerpig@gmail.com>
2023-08-25 16:51:42 +0700

    ssss

--- script_locust.py
+++ script_locust.py
@@ -1,11 +1,9 @@
 from locust import task, between
 from locust.contrib.fasthttp import FastHttpUser
-import requests
 
 class MyUser(FastHttpUser):
     wait_time = between(0.5, 1)
 
     @task
-    def download_file(self):
-        url = "URL_OF_YOUR_FILE"
-        response = requests.get(url)
+    def index(self):
+        response = self.client.get("/")
